e7dd7cefb8e2d9891d5e434ea6f5dc93ef06375e
Lucas Kuhn <lucasalexkuhn@gmail.com>
2022-11-17 16:50:14 -0300

    initial commit 2

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ MLXFLAGS	=	-lmlx -Ilmlx -lXext -lX11
 SRCS		=	main.c
 OBJ_DIR		=	obj
 OBJS		=	$(SRCS:%.c=$(OBJ_DIR)/%.o)
+LIBFT		=	libft/libft.a
+LIBFT_FLAGS	=	-L./libft -lft
 ifeq ($(shell uname),Darwin) # MacOS
 	MLXFLAGS	+=	-framework OpenGL -framework AppKit
 endif
@@ -10,13 +12,16 @@ endif
 all: $(NAME)
 
 $(OBJ_DIR)/%.o: %.c
-	$(CC) $(CFLAGS)  -c $< -o $@ -I ./
+	$(CC) $(CFLAGS) -c $< -o $@ -I ./
 
-$(NAME):  $(OBJ_DIR) $(OBJS)
-	$(CC) $(OBJS) $(MLXFLAGS) -o $(NAME)
+$(NAME): $(OBJ_DIR) $(OBJS) $(LIBFT)
+	$(CC) $(OBJS) $(MLXFLAGS) $(LIBFT_FLAGS) -o $(NAME)
 
 $(OBJ_DIR):
 	mkdir -p obj
+
+$(LIBFT):
+	make --directory=libft
 
 clean:
 	@rm -rf obj
--- main.c
+++ main.c
@@ -1,12 +1,62 @@
 # include <mlx.h> // MiniLibX
+# include <stdlib.h>
+#include <stdio.h> // printf
+#include "libft/libft.h" // libft
+
+# define KEY_ESC 65307
+# define BTN_X 17
+# define NO_EVENT 0L
+# define TRUE 1
+# define FALSE 0
+
+void			*mlx;
+void			*win;
+
+int	close_game()
+{
+	mlx_destroy_window(mlx, win);
+	exit(0);
+}
+
+int key_hook(int keycode)
+{
+	if (keycode == KEY_ESC)
+		close_game();
+	return (0);
+}
+
+int valid_extension(char *map_name)
+{
+	char	*extension;
+
+	extension = ft_strrchr(map_name, '.');
+	if (!extension || ft_strncmp(extension, ".cub", 4) != 0)
+		return (FALSE);
+	return (TRUE);
+}
+
+int is_valid(char *map_name)
+{
+	if (!valid_extension(map_name))
+		return (FALSE);
+	return(TRUE);
+}
 
 int	main(int argc, char **argv)
 {
-	void			*mlx;
-	void			*win;
+	char *map_name;
 
+	map_name = argv[1];
+
+	if (!is_valid(map_name))
+	{
+		printf("Invalid map: %s\n", map_name);
+		exit(1);
+	}
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 200, 200, "cub3D");
+	mlx_hook(win, BTN_X, NO_EVENT, close_game, NULL);
+	mlx_key_hook(win, key_hook, NULL);
 	mlx_loop(mlx);
 	return (0);
 }
