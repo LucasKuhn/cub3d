29748252910aba106640ed8bbdbad96fd0d678ba
Lucas Kuhn <lucasalexkuhn@gmail.com>
2022-11-24 15:51:23 -0300

    check for surrounging walls on map

--- main.c
+++ main.c
@@ -83,6 +83,14 @@ char	*load_map(char *map, int fd)
 	return (map);
 }
 
+// TODO: THIS IS TRASH! NOT OK! PLEASE FIX ME! 
+int found_NO = FALSE;
+int found_SO = FALSE;
+int found_WE = FALSE;
+int found_EA = FALSE;
+int found_F = FALSE;
+int found_C = FALSE;
+
 int	invalid_color(char *map)
 {
 	char	**colors;
@@ -95,6 +103,10 @@ int	invalid_color(char *map)
 	i = 0;
 	if (*map != 'F' && *map != 'C')
 		return (TRUE);
+	if (*map == 'F')
+		found_F = TRUE;
+	if (*map == 'C')
+		found_C = TRUE;
 	map++;
 	while (*map == ' ')
 		map++;
@@ -139,6 +151,14 @@ int	invalid_texture(char *map)
 		printf("Invalid identifier\n");
 		return (TRUE);
 	}
+	if (ft_strncmp(map, "NO", 2) == 0)
+		found_NO = TRUE;
+	if (ft_strncmp(map, "SO", 2) == 0)
+		found_SO = TRUE;
+	if (ft_strncmp(map, "WE", 2) == 0)
+		found_WE = TRUE;
+	if (ft_strncmp(map, "EA", 2) == 0)
+		found_EA = TRUE;
 	map += 2;
 	while (*map == ' ')
 		map++;
@@ -158,15 +178,163 @@ int	invalid_texture(char *map)
 	return (FALSE);
 }
 
+int any_identifier_missing(void)
+{
+	if (found_NO == FALSE)
+	{
+		printf("Missing NO\n");
+		return (TRUE);
+	}
+	if (found_SO == FALSE)
+	{
+		printf("Missing SO\n");
+		return (TRUE);
+	}
+	if (found_WE == FALSE)
+	{
+		printf("Missing WE\n");
+		return (TRUE);
+	}
+	if (found_EA == FALSE)
+	{
+		printf("Missing EA\n");
+		return (TRUE);
+	}
+	if (found_F == FALSE)
+	{
+		printf("Missing F\n");
+		return (TRUE);
+	}
+	if (found_C == FALSE)
+	{
+		printf("Missing C\n");
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+// TODO: Move this to utils 
+size_t	ft_arrlen(char **arr)
+{
+	size_t	i;
+
+	i = 0;
+	while (arr[i])
+		i++;
+	return (i);
+}
+
+int surrounded_by_walls(char *map)
+{
+	int i;
+	int j;
+
+	// Replace player for 0
+	i = 0;
+	while (map[i])
+	{
+		if (map[i] == 'N' || map[i] == 'S' || map[i] == 'E' || map[i] == 'W')
+			map[i] = '0';
+		i++;
+	}
+	// Turn map into matrix 
+	char **array;
+	array = ft_split(map, '\n');
+	int last_line = ft_arrlen(array) - 1;
+	// Iterate matrix and check if every 0 is surrounded by other 0 or 1
+	i = 0;
+	while (array[i])
+	{
+		j = 0;
+		while (array[i][j])
+		{
+			if (array[i][j] == '0')
+			{
+				// Can't have a 0 on the first line or the last line
+				if (i == 0 || i == last_line)
+					return (FALSE);
+				// Can't have an open space to the right
+				if (array[i][j + 1] != '1' && array[i][j + 1] != '0')
+					return (FALSE);
+				// Can't have an open space to the left
+				if (array[i][j - 1] != '1' && array[i][j - 1] != '0')
+					return (FALSE);
+				// Can't have an open space above
+				if (ft_strlen(array[i - 1]) < i)
+					return (FALSE);
+				if (array[i - 1][j] != '1' && array[i - 1][j] != '0')
+					return (FALSE);
+				// Can't have an open space below
+				if (ft_strlen(array[i + 1]) < i)
+					return (FALSE);
+				if (array[i + 1][j] != '1' && array[i + 1][j] != '0')
+					return (FALSE);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (TRUE);
+}
+
+int	has_invalid_characters(char *map)
+{
+	while (*map)
+	{
+		map++;
+		if (!*map)
+			return (FALSE);
+		if (*map == ' ' || *map == '\n')
+			continue;
+		if (*map == '0' || *map == '1')
+			continue;
+		if (*map == 'N' || *map == 'S' || *map == 'E' || *map == 'W')
+			continue;
+		printf("Invalid character:(%c)\n", *map);
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
+int has_starting_position(char *map)
+{
+	while (*map)
+	{
+		if (*map == 'N' || *map == 'S' || *map == 'E' || *map == 'W')
+			return (TRUE);
+		map++;
+	}
+	return (FALSE);
+}
+
+int	valid_map_tiles(char *map)
+{
+	if (has_invalid_characters(map))
+	{
+		printf("Contains invalid characters\n");
+		return(FALSE);
+	}
+	if (!has_starting_position(map))
+	{
+		printf("Starting position not present\n");
+		return(FALSE);
+	}
+	if (!surrounded_by_walls(map))
+		return(FALSE);
+	return(TRUE);
+}
+
 int	has_errors(char *map)
 {
 	int	identifier_size;
 
+	// Check all identifiers
 	while(*map != '\0')
 	{
 		while (*map && *map == '\n')
 			map++;
-		if (!*map)
+		// Exit the identifiers check when a map starts
+		if (*map == '1' || *map == ' ')
 			break;
 		identifier_size = 0;
 		while(map[identifier_size] && map[identifier_size] != ' ')
@@ -193,6 +361,13 @@ int	has_errors(char *map)
 		while (*map && *map != '\n')
 			map++;
 	}
+	if (any_identifier_missing())
+	{
+		printf("At least 1 identifier is missing\n");
+		return (TRUE);
+	}
+	if (!valid_map_tiles(map))
+		return (TRUE);
 	return (FALSE);
 }
 
